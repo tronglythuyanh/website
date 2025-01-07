c4d78e89ea896902f653ef8cf7ee39754b9d31bf
Trong <tronglyhieu1997@gmail.com>
2025-01-07 16:58:55 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Trong_Ly_Resume</title>
+        <link rel="stylesheet" href="../style/style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Trong Ly's Resume</h1>
+            <p>Software Engineer | 858 S Eliot St, Denver, CO, USA | 720-266-0614 | tronglyhieu1997@gmail.com</p>
+        </header>
+    </body>
